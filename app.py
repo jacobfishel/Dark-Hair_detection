88692bdc5fe7e9e4a4b0d82a19263e7a92d90094
Jacob Fishel <jacobfis8303@gmail.com>
2024-09-15 22:04:09 -0700

    in progress

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask #initialize app
 from views import views
 
-app = Flask(__name__)
+app = Flask(__name__)#initialize this file. This gives us an empty website with nothing.
 app.register_blueprint(views, url_prefix="/views")
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
+
--- views.py
+++ views.py
@@ -1,24 +1,58 @@
-from flask import Blueprint, render_template, request, jsonify, redirect, url_for
+# from flask import Blueprint, render_template, request, jsonify, redirect, url_for
+# '''Imports:
+#     flask: a web dev framework
+#     Blueprint: '''
 
-views = Blueprint(__name__, "views")
+# views = Blueprint("views", __name__)
+
+# @views.route("/")
+# def home():
+#     return render_template("index.html")
+
+# @views.route("/profile")
+# def profile():
+#     return render_template("profile.html")
+
+# @views.route("/json")
+# def get_json():
+#     return jsonify({'name': 'tim', 'coolness': 10})
+
+# @views.route("/data")
+# def get_data():
+#     data = request.json
+#     return jsonify(data)
+
+# @views.route("/go-to-home")
+# def go_to_home():
+#     return redirect(url_for("views.home"))
+
+
+
+
+
+
+
+
+from flask import Blueprint, render_template, redirect, url_for
+
+views = Blueprint("views", __name__) #creating a views blueprint
 
 @views.route("/")
 def home():
     return render_template("index.html")
 
-@views.route("/profile")
-def profile():
-    return render_template("profile.html")
+@views.route("/core_operations/")
+def core_operations():
+    return render_template("core_operations.html")
 
-@views.route("/json")
-def get_json():
-    return jsonify({'name': 'tim', 'coolness': 10})
+@views.route("/drawing_shapes_and_text_on_images/")
+def drawing_shapes_and_text_on_images():
+    return render_template("drawing_shapes_and_text_on_images.html")
 
-@views.route("/data")
-def get_data():
-    data = request.json
-    return jsonify(data)
+@views.route("/image_thresholding/")
+def image_thresholding():
+    return render_template("image_thresholding.html")
 
-@views.route("/go-to-home")
-def go_to_home():
-    return redirect(url_for("views.home"))
+@views.route("/edge_detection/")
+def edge_detection():
+    return render_template("edge_detection.html")
