d63bdddd387f43c32a43ec8d6a61770aa209e5d2
Jacob Fishel <jacobfis8303@gmail.com>
2024-10-25 10:09:54 -0700

    refresh

--- static/core-operations.js
+++ static/core-operations.js
@@ -1,3 +1,18 @@
-function printer() {
-    console.log("Hello World");
-}
+document.getElementById('imageInput').addEventListener('change', function(event) {
+    const imageContainer = document.getElementById('uploadedImage');
+    const file = event.target.files[0];
+
+    if (file) {
+        console.log("File");
+        const reader = new FileReader();
+
+        reader.onload = function(e) {
+            // Set the src attribute of the img tag to the uploaded file data
+            imageContainer.src = e.target.result;
+            imageContainer.style.display = 'block';  // Make the image visible
+        }
+
+        // Read the image file as a data URL
+        reader.readAsDataURL(file);
+    }
+});
